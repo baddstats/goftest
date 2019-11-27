c4cbdeb3c4511e4655de3f722fe60733498f224d
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2019-11-27 14:51:54 +1300

    Tweaked tolerances

--- R/cramer.R
+++ R/cramer.R
@@ -3,7 +3,7 @@
 ##
 ## Distribution of the Cramer-Von Mises test statistic
 ##
-## $Revision: 1.2 $ $Date: 2014/06/09 04:34:49 $
+## $Revision: 1.9 $ $Date: 2019/11/27 01:50:20 $
 ##
 ## ..................................................................
 ##
@@ -36,6 +36,7 @@ pCvM <- local({
   ED3 <- function(x) { exp(-(x^2)/4) * D3(x) }
 
   Ak <- function(k, x) {
+    #' original code (transliterated from Matlab) for reference
     twosqrtx <- 2 * sqrt(x)
     x34 <- x^(3/4)
     x54 <- x^(5/4)
@@ -46,17 +47,46 @@ pCvM <- local({
             7*(2*k+1)*gamma(k+1/2)*ED2((4*k+5)/twosqrtx)/(144*x34)
   }
 
+  AkOnFk <- function(k, x) {
+    #' calculates A(k, x)/factorial(k)
+    #' Adrian Baddeley, 26 nov 2019
+    twosqrtx <- 2 * sqrt(x)
+    fk1x <- (4*k+1)/twosqrtx
+    fk3x <- (4*k+3)/twosqrtx
+    fk5x <- (4*k+5)/twosqrtx
+    x34 <- x^(3/4)
+    x54 <- x^(5/4)
+    #'evaluate gamma(k+1/2)/factorial(k) = gamma(k+1/2)/gamma(k+1)
+    gf <- if(k < 100) {
+            gamma(k+1/2)/factorial(k)
+          } else if(k <= 1e15) {
+            exp(lgamma(k+1/2)-lgamma(k+1))
+          } else exp(-10*k)
+    gf * (
+      ED3(fk1x)/(72*x54) +
+      (2*k+1) * (
+        ED2(fk3x)/(9*x34) +
+        (2*k+3)*ED3(fk5x)/(12*x54) +
+        7*(ED2(fk1x)+ED2(fk5x))/(144*x34)
+      )
+    )
+  }
+
   psi1 <- function(x) {
     ## Leading term in expansion of small-sample cdf of Cramer-Von Mises
     m <- length(x)
     tot <- numeric(m)
     active <- rep(TRUE, m)
-    for(k in 0:20) {
-      z <- -Ak(k,x[active])/(pi*factorial(k))
+    for(k in 0:200) {
+      ## WAS:      z <- -Ak(k,x[active])/(pi*factorial(k))
+      z <- -AkOnFk(k,x[active])/pi
       tot[active] <- tot[active] + z
-      active[active] <- (abs(z) >= 1e-7)
-      if(!any(active)) break
+      active[active] <- (abs(z) >= 1e-9)
+      if((k > 20) && (ok <- !any(active))) break
     }
+    if(!ok)
+      warning("Series did not converge after 200 iterations (small sample cdf)",
+              call.=FALSE)
     return(tot + Vinf(x)/12)
   }
 
@@ -65,14 +95,17 @@ pCvM <- local({
     m <- length(x)
     tot <- numeric(m)
     active <- rep(TRUE, m)
-    for(k in 0:10) {
+    for(k in 0:200) {
       q <- (4*k+1)^2/(16*x[active])
       z <- ((-1)^k)*choose(-1/2,k)*sqrt(4*k+1)*
         exp(-q)*besselK(q, nu=1/4)/sqrt(x[active])
       tot[active] <- tot[active] + z
-      active[active] <- (abs(z) >= 1e-7)
-      if(!any(active)) break
+      active[active] <- (abs(z) >= 1e-9)
+      if((k > 10) && (ok <- !any(active))) break
     }
+    if(!ok)
+      warning("Series did not converge after 200 iterations (asymptotic cdf)",
+              call.=FALSE)
     return(tot/pi)
   }
 
@@ -84,13 +117,9 @@ pCvM <- local({
     
   pCvM <- function(q, n=Inf, lower.tail=TRUE) {
     ## cdf of null distribution of Cramer-von Mises test statistic
-    if(is.finite(n)) {
-      lower <- 1/(12 * n)
-      upper <- n/3
-    } else {
-      lower <- 0
-      upper <- Inf
-    }
+    nn <- min(100, n)
+    lower <- 1/(12 * nn)
+    upper <- nn/3
     m <- length(q)
     p <- numeric(m)
     unknown <- rep(TRUE, m)
@@ -104,6 +133,8 @@ pCvM <- local({
     }
     if(any(unknown))
       p[unknown] <- if(is.infinite(n)) Vinf(q[unknown]) else Vn(q[unknown], n)
+    p[p < 2e-10] <- 0
+    p[(1-p) < 2e-10] <- 1
     return(if(lower.tail) p else 1-p)
   }
 
@@ -124,9 +155,11 @@ qCvM <- local({
     lower <- if(is.finite(n)) (1/(12 * n)) else 0
     upper <- if(is.finite(n)) n/3 else Inf
     ans <- numeric(length(p))
-    ans[p == 0] <- lower
-    ans[p == 1] <- upper
-    for(i in which(p > 0 & p < 1)) 
+    small <- (p <= 2e-10)
+    large <- (1-p <= 2e-10)
+    ans[small] <- lower
+    ans[large] <- upper
+    for(i in which(!small & !large))
       ans[i] <- uniroot(f, c(lower, 1), N=n, P=p[i], extendInt="up")$root
     return(ans)
   }
